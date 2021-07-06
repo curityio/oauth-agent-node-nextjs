3de8508de936f0a84b4a99b0ed0e20638d54b7a7
Michal Trojanowski <michal@curity.io>
2021-07-06 12:07:14 +0200

    Redirect to login

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,12 @@
 export default (req, res) => {
     res.json({
-        cookies: req.cookies
+        location: "https://trojan.ngrok.io/dev/oauth/authorize?\n" +
+            "&client_id=michal-test-www\n" +
+            "&state=1586511942384-OcG\n" +
+            "&scope=openid\n" +
+            "&response_type=code\n" +
+            "&code_challenge=ERNHshyzhznDQOKAIEkJl94N048wMAaN4jY-2xlVy_s\n" +
+            "&code_challenge_method=S256\n" +
+            "&redirect_uri=https://oauth.tools/callback/code"
     });
 }
